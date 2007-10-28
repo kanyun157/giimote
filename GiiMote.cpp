a538fca943c39ae06751a88340d73c312db00f0f
leif902 <leif902@localhost>
2007-10-28 02:22:13 +0000

    Added items remotely
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\mii.h
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\Debug
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\app.rc
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\AssemblyInfo.cpp
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\GiiMote.cpp
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\GiiMote.h
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\GiiMote.vcproj
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\GiiMote.vcproj.LEIF.Leif902.user
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\ReadMe.txt
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\resource.h
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\WiimoteLib.dll
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\Stdafx.cpp
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\Stdafx.h
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\Release
    C:\Documents and Settings\Leif902\My Documents\Visual Studio 2005\Projects\GiiMote\GiiMote\app.ico

---
+++ GiiMote.cpp
@@ -0,0 +1,12 @@
+// This is the main DLL file.
+
+/*
+TODO:
+	Fix bugs
+	Add sound and speaker functions
+	Add higher level functionality so the user dosn't have to do it
+	Add calibration writing functions
+*/
+
+#include "stdafx.h"
+#include "GiiMote.h"
