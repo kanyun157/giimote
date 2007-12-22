b1d3025f2efdda7bbfb69b225ca6053585f028b1
leif902 <leif902@localhost>
2007-12-22 04:21:42 +0000

    Re-added GiiMote source folder.

---
+++ GiiMote/GiiMote.cpp
@@ -0,0 +1,14 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+#include "GiiMote.h"
+
+#include "mii.h"
+#include "GMMath.h"
+#include "Wiimote.h"
+#include "GMButtons.h"
+#include "Joystick.h"
+#include "GMAccel.h"
+#include "IR.h"
+#include "Nunchuck.h"
+#include "Classic.h"
