ba78593ab42b807e0b267daf9148ae31a40c0788
leif902 <leif902@localhost>
2008-04-18 00:34:13 +0000

    Updated Roll and Yaw calculations.

--- GiiMote/Orientation.h
+++ GiiMote/Orientation.h
@@ -7,33 +7,32 @@ namespace GiiMoteLib {
     /////////////////////////
 
     /// <summary>The roll of the Wii Remote</summary>
-    /// <returns>Roll in degrees</returns>
+    /// <returns>Roll in degrees or -1000 on error.</returns>
     double GiiMote::wm_get_roll()
     {
 		double roll = 0;
 
-		if (Math::Abs(wmState->AccelState.X) + Math::Abs(wmState->AccelState.Y) + Math::Abs(wmState->AccelState.Z) <= 1.15)
+		try
 		{
-			try
-			{
-				roll = -Math::Atan2(double(wmState->AccelState.X), Math::Sqrt( Math::Pow(double(wmState->AccelState.Y), 2) + Math::Pow(double(wmState->AccelState.Z), 2)) );
-			}
-			catch(...)
-			{
-				// Do Nothing
-			}
+			roll = -Math::Atan2(double(wmState->AccelState.X), Math::Sqrt( Math::Pow(double(wmState->AccelState.Y), 2) + Math::Pow(double(wmState->AccelState.Z), 2)) ) - Math::PI/2;
 		}
-		else
+		catch(...)
 		{
+			// If the Wii Remote is accelerating, use the IR.
 			if (this->wmState->IRState.Found1 && this->wmState->IRState.Found2)
 			{
+				Math::Atan2(this->wmState->IRState.Y2 - this->wmState->IRState.Y1, this->wmState->IRState.X2 - this->wmState->IRState.X1);
+			}
+			else
+			{
+				return ( -1000 );
 			}
 		}
 
 		return ( roll * (180 / Math::PI) );
     }
     /// <summary>The pitch of the Wii Remote</summary>
-    /// <returns>Pitch in degrees</returns>
+    /// <returns>Pitch in degrees or -1000 on error</returns>
     double GiiMote::wm_get_pitch()
     {
 		double pitch = 0;
@@ -44,29 +43,32 @@ namespace GiiMoteLib {
 		}
 		catch(...)
 		{
-			return ( 0 );
+			return ( -1000 );
 		}
 
 		return ( pitch * (180 / Math::PI) );
     }
 
     /// <summary>The yaw of the Wii Remote</summary>
-    /// <returns>Yaw in degrees</returns>
+    /// <returns>Yaw in degrees or -1000 on error</returns>
     double GiiMote::wm_get_yaw()
     {
 		double yaw = 0;
 
 		try
 		{
-			if (this->wmState->IRState.Found1)
+			if (this->wmState->IRState.Found1 && this->wmState->IRState.Found2)
 			{
 				// This is not right at all...
-				yaw = Math::Atan2(512 - this->wmState->IRState.RawX1, 600);
+				double ax = ((double)this->wmState->IRState.RawX1 + (double)this->wmState->IRState.RawX2) / 2;
+				double z = 1023 - Math::Sqrt(Math::Pow((double)this->wmState->IRState.RawX2 - (double)this->wmState->IRState.RawX2, 2) + Math::Pow((double)this->wmState->IRState.RawY2 - (double)this->wmState->IRState.RawY2, 2));
+				yaw = Math::Atan2((ax - 512) * (z / 1024), z);
+				// yaw = Math::Atan2(512 - this->wmState->IRState.RawX1, 600);
 			}
 		}
 		catch(...)
 		{
-			return ( -1 );
+			return ( -1000 );
 		}
 
 		return ( yaw * (180 / Math::PI) );
