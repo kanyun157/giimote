45d012c67a2dcd7cd6359e79ede4d3df680a6f91
leif902 <leif902@localhost>
2008-04-26 14:55:31 +0000

    Minor updates

--- Help/GiiMote Documentation/Working/Generate1xTOC.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-REM Step 4.2 - Generate a table of content for an HTML 1.x help file
-"C:\Program Files\Sandcastle\ProductionTools\XslTransform" /xsl:"C:\Program Files\Sandcastle\ProductionTransforms\TocToChmContents.xsl" toc.xml /out:"Documentation.hhc"
--- Help/GiiMote Documentation/Working/GenerateRefInfo.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-REM Step 1 - Generate the reflection information
-"C:\Program Files\Sandcastle\ProductionTools\MRefBuilder" /config:MRefBuilder.config /out:reflection.org /dep:DLL\*.*  *.dll *.exe
